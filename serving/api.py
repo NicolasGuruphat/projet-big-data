1dd9ef75326cdbff450820780ea386309551499b
guillaume GRANDY <guillaume.grandy@gmail.com>
2025-03-18 11:15:30 +0100

    ta mère

--- serving/api.py
+++ serving/api.py
@@ -1,15 +1,19 @@
-import fastapi
-import pickle
+from fastapi import FastAPI, UploadFile, File
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+from pickle import *
 
-app = fastapi.FastAPI()
+app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"Routes: /predict, /feedback"}
 
 @app.post("/predict")
-def predict(data: fastapi.UploadFile = fastapi.File(...)):
-    return model.predict(data)
+def predict(data: UploadFile = File(...)):
+    #json_compatible_item_data = jsonable_encoder(model.predict(data))
+    json_compatible_item_data = jsonable_encoder("Chien")
+    return JSONResponse(content=json_compatible_item_data)
 
 @app.post("/feedback")
 def feedback(data: bool):
